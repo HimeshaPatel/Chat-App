24b94bdee248b56c41c9f00461f2082a7f542b84
himesha <himesha.patel7@gmail.com>
2024-12-22 19:20:31 +1100

    "conversations-done"

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import { BsSend } from "react-icons/bs"; 
+import useSendMessage from "../../hooks/useSendMessage";
 const MessageInput = () => {
+
+  const [message, setMessage] = useState();
+  const{loading, sendMessage} =useSendMessage();
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    if(!message) return;
+    await sendMessage(message);
+    setMessage("");
+    console.log(message);
+  }
+
   return (
-    <form className="px-4 my-3">
+    <form className="px-4 my-3" onSubmit={handleSubmit}>
         <div className="w-full relative">
-        <input type="text" placeholder="Send a message..." className="border border-slate-400 p-2.5 text-sm
+        <input type="text" placeholder="Send a message..."        
+        value={message}
+        onChange={(e) => setMessage(e.target.value)} 
+        className="border border-slate-400 p-2.5 text-sm
          rounded-lg block bg-gray-700 border-gray-700 text-white w-full" />
             <button type="submit" className="absolute inset-y-0 end-0 flex items-center pe-3">
-                <BsSend />
+               
+                {loading ? <span className="loading loading-spinner text-white"></span> :  <BsSend />}
             </button>
         </div>
     </form>
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,15 +1,25 @@
 import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emoji";
 import Conversation from "./Conversation"
 
 const Conversations = () => {
 
   
   const { loading, conversations } = useGetConversations();
-  console.log("conversations",conversations);
+  console.log("conversations", conversations);
 
   return (
     <div className="py-2 flex flex-col overflow-auto">
-        <Conversation />
+       {
+         conversations.map((conversation, idx) => (
+           <Conversation key={conversation._id} 
+           conversation={conversation} 
+           emoji={getRandomEmoji()}
+           lastIdx = {idx === conversations.length - 1}
+           />
+         ))
+       }
+       {loading ? <span className="loading loading-spinner mx-auto"></span> : null}
     </div>
   )
 }
